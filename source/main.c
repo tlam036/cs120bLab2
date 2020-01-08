4418e4380ff92a0e843f81fb9a63089cde925f7d
tlam036 <56003187+tlam036@users.noreply.github.com>
2020-01-07 20:51:47 -0800

    fix indentation

--- source/main.c
+++ source/main.c
@@ -1,11 +1,11 @@
-/*	Author: tlam036
+/*  Author: tlam036
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *  Lab Section: 024
+ *  Assignment: Lab #  Exercise #
+ *  Exercise Description: [optional - include for your own benefit]
  *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
+ *  I acknowledge all content contained herein, excluding template or example
+ *  code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
@@ -17,11 +17,11 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
 				// Initialize output on PORTB to 0x00
-				 unsigned char temp_input = 0x00;
-				 while(1) {
-				 	temp_input = PINA;
-				 		PORTB = temp_input;	// Writes port B's 8 pins with the values
-				 						// on port A's 8 pins
-				 						}
-				 						return 1;
+	unsigned char temp_input = 0x00;
+	while(1) {
+		temp_input = PINA;
+		PORTB = temp_input;	// Writes port B's 8 pins with the values
+				 	// on port A's 8 pins
+	}
+	return 1;
 }
